cfb8cbebccebf05bf6bbbd0aa153928991d2e6a2
Arpan Baruah <arpanbaruah.83@gmail.com>
2017-08-25 13:53:30 -0700

    TestCode Swift

--- DaProject/ViewController.swift
+++ DaProject/ViewController.swift
@@ -3,17 +3,33 @@
 //  DaProject
 //
 //  Created by ARPAN BARUAH on 8/25/17.
-//  Copyright © 2017 ARPAN BARUAH. All rights reserved.
+//  Copyright © 2017 ARPAN BARUAH. All rights reserved..
 //
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var daLabel: UILabel!
+    var tapCount = 0
+    @IBAction func daButton(_ sender: Any) {
+        
+        tapCount = tapCount + 1
+        if(tapCount > 10){
+            daLabel.text = "You pressed the button 10 times"
+        }
+        
+        
+       //  daLabel.text = "Buttons are cool!"
+    }
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+       
+         self.view.backgroundColor = UIColor.lightGray
     }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
