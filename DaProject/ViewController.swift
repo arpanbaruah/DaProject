80e4bfa891e2060ccd349ede1289f3d6fa8df65c
Arpan Baruah <arpanbaruah.83@gmail.com>
2017-08-27 19:19:45 -0700

    new changes

--- DaProject/ViewController.swift
+++ DaProject/ViewController.swift
@@ -11,16 +11,20 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var daLabel: UILabel!
+    @IBOutlet weak var text1: UITextField!
     var tapCount = 0
+    @IBOutlet weak var text2: UITextField!
     @IBAction func daButton(_ sender: Any) {
-        
+       /*
         tapCount = tapCount + 1
         if(tapCount > 10){
             daLabel.text = "You pressed the button 10 times"
         }
-        
+        */
         
        //  daLabel.text = "Buttons are cool!"
+        daLabel.text = "\(Double(text1.text!)! * Double(text2.text!)!)"
+        
     }
     override func viewDidLoad() {
         
